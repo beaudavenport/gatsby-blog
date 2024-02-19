5a432d5196fe52e2e3df6d4ab8c5d1eeba5cca11
Beau Davenport <beaudavenport@Beaus-MBP.attlocal.net>
2024-02-19 12:39:04 -0600

    Updates

---
+++ component---src-pages-404-js-c25f02ed5fe6bce71711.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_blog=self.webpackChunkgatsby_blog||[]).push([[125],{3331:function(e,t,l){l.r(t);var n=l(6540),a=l(492),s=l(2269);t.default=()=>n.createElement(a.A,null,n.createElement(s.A,{title:"404: Not found"}),n.createElement("h1",null,"NOT FOUND"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}]);
+//# sourceMappingURL=component---src-pages-404-js-c25f02ed5fe6bce71711.js.map
